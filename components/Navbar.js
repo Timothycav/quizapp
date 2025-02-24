f04c58eb6d9445df147955c4b3b1e57a8f93efd1
Timothy Cavinder <Timothycavinder@msn.com>
2025-02-24 09:41:56 -0600

    media q's updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,32 +1,38 @@
+import { useState } from "react";
 import Link from 'next/link';
 import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className={ styles.navbar}>
-      <ul>
+       {/* Hamburger Icon */}
+     <button className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
+                    {isOpen ? "✖" : "☰"}
+                </button>
+      <ul className={`${styles.navLinks} ${isOpen ? styles.show : ""}`}>
         <li>
-          <Link href="/">
+          <Link href="/" onClick={() => setIsOpen(false)}>
             Home
           </Link>
         </li>
         <li>
-          <Link href="/html">
+          <Link href="/html" onClick={() => setIsOpen(false)}>
             HTML
           </Link>
         </li>
         <li>
-          <Link href="/css">
+          <Link href="/css" onClick={() => setIsOpen(false)}>
             CSS
           </Link>
         </li>
         <li>
-          <Link href="/js">
+          <Link href="/js" onClick={() => setIsOpen(false)}>
             JS
           </Link>
         </li>
         <li>
-          <Link href="/acc">
+          <Link href="/acc" onClick={() => setIsOpen(false)}>
             ACC
           </Link>
         </li>
